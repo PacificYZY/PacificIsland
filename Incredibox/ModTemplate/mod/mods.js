cb34b0568f08db5b850e57c370de94ee285361ef
PacificYZY <152719868+PacificYZY@users.noreply.github.com>
2025-09-06 18:19:50 +0800

    Add files via upload

---
+++ Incredibox/ModTemplate/mod/mods.js
@@ -0,0 +1,30 @@
+modsList = [
+	{
+		"name": "Cold wave",
+		"text": "简介：Cold wave是一款以冷酷、简约为主题的音乐盒子应用。它提供了丰富的音乐资源和个性化的功能，让用户可以轻松地创建和分享自己的音乐作品。",
+		"icon": "mod/img/yourTeam.jpg",
+		"apk": "https://www.mediafire.com/file/za7ytocj2oxjq37/lofterbox_Coldwave.apk/file",
+		"zip": "https://www.mediafire.com/file/6601ikx36078i4s/lofterbox_Coldwave.zip/file",
+	},
+	{
+		"name": "Cold wave",
+		"text": "简介：Cold wave是一款以冷酷、简约为主题的音乐盒子应用。它提供了丰富的音乐资源和个性化的功能，让用户可以轻松地创建和分享自己的音乐作品。",
+		"icon": "mod/img/yourTeam.jpg",
+		"apk": "https://www.mediafire.com/file/za7ytocj2oxjq37/lofterbox_Coldwave.apk/file",
+		"zip": "https://www.mediafire.com/file/6601ikx36078i4s/lofterbox_Coldwave.zip/file",
+	},
+	{
+		"name": "Cold wave",
+		"text": "简介：Cold wave是一款以冷酷、简约为主题的音乐盒子应用。它提供了丰富的音乐资源和个性化的功能，让用户可以轻松地创建和分享自己的音乐作品。",
+		"icon": "mod/img/yourTeam.jpg",
+		"apk": "https://www.mediafire.com/file/za7ytocj2oxjq37/lofterbox_Coldwave.apk/file",
+		"zip": "https://www.mediafire.com/file/6601ikx36078i4s/lofterbox_Coldwave.zip/file",
+	},
+	{
+		"name": "Cold wave",
+		"text": "简介：Cold wave是一款以冷酷、简约为主题的音乐盒子应用。它提供了丰富的音乐资源和个性化的功能，让用户可以轻松地创建和分享自己的音乐作品。",
+		"icon": "mod/img/yourTeam.jpg",
+		"apk": "https://www.mediafire.com/file/za7ytocj2oxjq37/lofterbox_Coldwave.apk/file",
+		"zip": "https://www.mediafire.com/file/6601ikx36078i4s/lofterbox_Coldwave.zip/file",
+	},
+];
