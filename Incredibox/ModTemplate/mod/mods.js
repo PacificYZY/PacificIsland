69968e3e8d078888490cd2b36fe20bdc110f37c0
PacificYZY <152719868+PacificYZY@users.noreply.github.com>
2025-09-06 18:22:23 +0800

    Update mods.js

--- Incredibox/ModTemplate/mod/mods.js
+++ Incredibox/ModTemplate/mod/mods.js
@@ -27,4 +27,20 @@ modsList = [
 		"apk": "https://www.mediafire.com/file/za7ytocj2oxjq37/lofterbox_Coldwave.apk/file",
 		"zip": "https://www.mediafire.com/file/6601ikx36078i4s/lofterbox_Coldwave.zip/file",
 	},
+
 ];
+
+modsShow += "<div id='mods-show-content'><div class='title' id='show-title'>✨New Mods✨</div><div id='box-mods'><div id='slide-mods'>";
+let loadMods = "";
+var modsList = "";
+for (let i = 0; i < modsList.length; i++) {
+  loadMods += "<div class='box-show-mod'>";
+  loadMods += "<div class='mod-icon'><img src='" + modsList[i]["icon"] + "'/></div>";
+  loadMods += "<div class='title'>" + modsList[i]["name"] + "</div>";
+  loadMods += "<div class='txt'>" + modsList[i]["text"] + "</div>";
+  loadMods += "<button class='link-apk' onclick=\"openExternal('" + modsList[i]["apk"] + "')\"><span>APK</span></button>";
+  loadMods += "<button class='link-zip' onclick=\"openExternal('" + modsList[i]["zip"] + "')\"><span>ZIP</span></button>";
+  loadMods += "</div>";
+}
+modsShow += loadMods;
+modsShow += "</div></div></div><div class='mod-back'><div class='round' onclick='showContentBack()'><img src='mod/icon/back.svg' alt='SVG Image' width='100%'/></div></div><div class='mod-next'><div class='round' onclick='showContentNext()'><img src='mod/icon/next.svg' alt='SVG Image' width='100%'/></div></div>";
